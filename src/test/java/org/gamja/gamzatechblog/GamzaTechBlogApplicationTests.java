878873ab9afbb129a07b83f52ecd955586b11e86
박지훈 <jihun6548@naver.com>
2025-05-04 23:10:46 +0900

    feat: 초기 셋팅

---
+++ src/test/java/org/gamja/gamzatechblog/GamzaTechBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gamja.gamzatechblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamzaTechBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
