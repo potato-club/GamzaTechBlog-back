af28c9a985e4a390b1feb03476418ca2031e60d0
박지훈 <jihun6548@naver.com>
2025-06-16 16:20:15 +0900

    feat(comment): 본인 댓글 조회 API 구현 (서비스, 컨트롤러)

--- src/main/java/org/gamja/gamzatechblog/domain/comment/service/CommentService.java
+++ src/main/java/org/gamja/gamzatechblog/domain/comment/service/CommentService.java
@@ -2,9 +2,12 @@ package org.gamja.gamzatechblog.domain.comment.service;
 
 import java.util.List;
 
+import org.gamja.gamzatechblog.common.dto.PagedResponse;
 import org.gamja.gamzatechblog.domain.comment.model.dto.request.CommentRequest;
+import org.gamja.gamzatechblog.domain.comment.model.dto.response.CommentListResponse;
 import org.gamja.gamzatechblog.domain.comment.model.dto.response.CommentResponse;
 import org.gamja.gamzatechblog.domain.user.model.entity.User;
+import org.springframework.data.domain.Pageable;
 
 public interface CommentService {
 	List<CommentResponse> getCommentsByPostId(Long postId);
@@ -12,4 +15,7 @@ public interface CommentService {
 	CommentResponse createComment(User currentUser, Long postId, CommentRequest commentRequest);
 
 	void deleteComment(User currentUser, Long commentId);
+
+	PagedResponse<CommentListResponse> getMyComments(User user, Pageable pageable);
+
 }
