1e0718f899ee0d42c6fdd32969f13b4602bf5790
박지훈 <jihun6548@naver.com>
2025-06-16 15:46:52 +0900

    feat: 내 글 페이징 조회 API 추가

--- src/main/java/org/gamja/gamzatechblog/domain/post/repository/PostRepository.java
+++ src/main/java/org/gamja/gamzatechblog/domain/post/repository/PostRepository.java
@@ -3,6 +3,9 @@ package org.gamja.gamzatechblog.domain.post.repository;
 import java.util.Optional;
 
 import org.gamja.gamzatechblog.domain.post.model.entity.Post;
+import org.gamja.gamzatechblog.domain.user.model.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -10,4 +13,7 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 	@EntityGraph(attributePaths = {"user", "postTags", "postTags.tag"})
 		//leftJoin 쿼리 엔티티임
 	Optional<Post> findById(Long postId);
+
+	Page<Post> findByUserOrderByCreatedAtDesc(User user, Pageable pageable);
+
 }
