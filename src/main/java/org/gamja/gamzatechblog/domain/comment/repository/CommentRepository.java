aecd6dffc56434c66b450c095ab2f3d60552079c
박지훈 <jihun6548@naver.com>
2025-06-16 16:07:30 +0900

    feat(repository): User 기준 게시글/댓글/좋아요 카운트 메서드 추가

--- src/main/java/org/gamja/gamzatechblog/domain/comment/repository/CommentRepository.java
+++ src/main/java/org/gamja/gamzatechblog/domain/comment/repository/CommentRepository.java
@@ -3,9 +3,13 @@ package org.gamja.gamzatechblog.domain.comment.repository;
 import java.util.List;
 
 import org.gamja.gamzatechblog.domain.comment.model.entity.Comment;
+import org.gamja.gamzatechblog.domain.user.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
 	List<Comment> findAllByPostIdAndParentIsNullOrderByCreatedAtAsc(Long postId);
+
+	int countByUser(User user);
+
 }
