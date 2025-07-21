672e996db47d1c7206ceca6644378b6068282b18
박지훈 <jihun6548@naver.com>
2025-07-21 14:46:16 +0900

    hotfix: ProfileImage클래스 중복 삭제

--- src/main/java/org/gamja/gamzatechblog/domain/profile/model/entity/ProfileImage.java
+++ src/main/java/org/gamja/gamzatechblog/domain/profile/model/entity/ProfileImage.java
@@ -1,18 +1,39 @@
 package org.gamja.gamzatechblog.domain.profile.model.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
+import org.gamja.gamzatechblog.common.entity.BaseTime;
+import org.gamja.gamzatechblog.domain.user.model.entity.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "profile_images")
-@Getter @NoArgsConstructor @AllArgsConstructor @Builder
-public class ProfileImage {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "profile_image_id")
-    private Long id;
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class ProfileImage extends BaseTime {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "profile_image_id")
+	private Long id;
 
-    @Column(name = "url", nullable = false)
-    private String url;
+	@OneToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "user_id")
+	private User user;
 
+	@Column(name = "profile_image_url", length = 255, nullable = false)
+	private String profileImageUrl;
 }
