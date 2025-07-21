a78cdc00c0bd2db55de705c45a8b81c730f96c49
박지훈 <jihun6548@naver.com>
2025-07-21 11:04:00 +0900

    feat: Image엔티티 로직 분리 Profile, Post

---
+++ src/main/java/org/gamja/gamzatechblog/domain/image/model/entity/ProfileImage.java
@@ -0,0 +1,39 @@
+package org.gamja.gamzatechblog.domain.image.model.entity;
+
+import org.gamja.gamzatechblog.common.entity.BaseTime;
+import org.gamja.gamzatechblog.domain.user.model.entity.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "profile_images")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class ProfileImage extends BaseTime {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "profile_image_id")
+	private Long id;
+
+	@OneToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@Column(name = "image_url", length = 255, nullable = false)
+	private String profileImageUrl;
+}
