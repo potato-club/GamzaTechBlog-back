bb037d5b08d79ca267e2b7e791677240c263aedb
박지훈 <jihun6548@naver.com>
2025-07-23 14:23:27 +0900

    feat: User Profile 연관관계 설정

--- src/main/java/org/gamja/gamzatechblog/domain/user/model/entity/User.java
+++ src/main/java/org/gamja/gamzatechblog/domain/user/model/entity/User.java
@@ -23,7 +23,6 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
@@ -91,8 +90,7 @@ public class User extends BaseTime {
 	@Builder.Default
 	private List<Project> projects = new ArrayList<>();
 
-	@OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
-	@JoinColumn(name = "profile_image_id")
+	@OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
 	private ProfileImage profileImage;
 
 	@OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
