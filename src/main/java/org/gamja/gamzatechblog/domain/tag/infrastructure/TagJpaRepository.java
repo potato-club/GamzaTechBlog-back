d23021f193bafcc997839457b1a21f2b6ce2df85
박지훈 <jihun6548@naver.com>
2025-07-26 15:56:37 +0900

    fix: TagJpa에 트랙잭션 임폴트 변경

--- src/main/java/org/gamja/gamzatechblog/domain/tag/infrastructure/TagJpaRepository.java
+++ src/main/java/org/gamja/gamzatechblog/domain/tag/infrastructure/TagJpaRepository.java
@@ -8,8 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import jakarta.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface TagJpaRepository extends JpaRepository<Tag, Long> {
