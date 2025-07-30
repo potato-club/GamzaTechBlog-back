510b96f6a343f83a26c91b7be69b815ba0438dc1
박지훈 <jihun6548@naver.com>
2025-07-30 17:02:30 +0900

    feat(config): Multipart+JSON 컨버터 추가 및 WebConfig 설정

--- src/main/java/org/gamja/gamzatechblog/core/config/MultipartJackson2HttpMessageConverter.java
+++ src/main/java/org/gamja/gamzatechblog/core/config/MultipartJackson2HttpMessageConverter.java
@@ -1,4 +1,29 @@
 package org.gamja.gamzatechblog.core.config;
 
-public class MultipartJackson2HttpMessageConverter {
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Component
+public class MultipartJackson2HttpMessageConverter extends AbstractJackson2HttpMessageConverter {
+
+	public MultipartJackson2HttpMessageConverter(ObjectMapper mapper) {
+		super(mapper,
+			MediaType.APPLICATION_JSON,
+			MediaType.APPLICATION_OCTET_STREAM);
+	}
+
+	@Override
+	protected boolean canRead(MediaType mediaType) {
+		return MediaType.MULTIPART_FORM_DATA.includes(mediaType)
+			|| MediaType.APPLICATION_JSON.includes(mediaType)
+			|| MediaType.APPLICATION_OCTET_STREAM.includes(mediaType);
+	}
+
+	@Override
+	protected boolean canWrite(MediaType mediaType) {
+		return false;
+	}
 }
