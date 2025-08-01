643edda9d3490734fbd5dd1477758b7106dfb351
박지훈 <jihun6548@naver.com>
2025-08-02 03:35:44 +0900

    hoxfix: 댓글 직렬화 문제 해결

--- src/main/java/org/gamja/gamzatechblog/domain/comment/model/dto/response/CommentResponse.java
+++ src/main/java/org/gamja/gamzatechblog/domain/comment/model/dto/response/CommentResponse.java
@@ -3,10 +3,14 @@ package org.gamja.gamzatechblog.domain.comment.model.dto.response;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class CommentResponse {
 	private Long commentId;
