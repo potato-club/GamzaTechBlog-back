bb9cace3baa0918624adbbb5c71c21e3793a8316
박지훈 <jihun6548@naver.com>
2025-08-05 01:17:21 +0900

    refactor: 사용자 응답 DTO를 record로 변환하고 Swagger @Schema 추가

--- src/main/java/org/gamja/gamzatechblog/domain/user/controller/response/UserProfileResponse.java
+++ src/main/java/org/gamja/gamzatechblog/domain/user/controller/response/UserProfileResponse.java
@@ -1,5 +1,6 @@
 package org.gamja.gamzatechblog.domain.user.controller.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,15 +11,37 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserProfileResponse {
+
+	@Schema(description = "Github 아이디", example = "parkjihun")
 	private String githubId;
+
+	@Schema(description = "닉네임", example = "지훈")
 	private String nickname;
+
+	@Schema(description = "이름", example = "박지훈")
 	private String name;
+
+	@Schema(description = "이메일", example = "jihun@example.com")
 	private String email;
+
+	@Schema(description = "프로필 이미지 URL", example = "https://example-bucket.s3.amazonaws.com/profile.jpg")
 	private String profileImageUrl;
+
+	@Schema(description = "직책/포지션", example = "BACKEND")
 	private String position;
+
+	@Schema(description = "유저 역할", example = "USER")
 	private String role;
+
+	@Schema(description = "학번", example = "20231234")
 	private String studentNumber;
+
+	@Schema(description = "감자 동아리 기수", example = "9")
 	private Integer gamjaBatch;
+
+	@Schema(description = "생성 일시", example = "2025-08-04T15:30:00")
 	private String createdAt;
+
+	@Schema(description = "수정 일시", example = "2025-08-05T10:20:30")
 	private String updatedAt;
 }
