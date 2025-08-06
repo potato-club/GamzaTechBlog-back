c1c55d663dfc779d0acae7697dd6ab5c9cc14e2b
박지훈 <jihun6548@naver.com>
2025-08-06 10:24:58 +0900

    refactor: Tag 스웨거 컨트롤러 문서화

--- src/main/java/org/gamja/gamzatechblog/domain/tag/controller/TagController.java
+++ src/main/java/org/gamja/gamzatechblog/domain/tag/controller/TagController.java
@@ -4,19 +4,18 @@ import java.util.List;
 
 import org.gamja.gamzatechblog.common.dto.ResponseDto;
 import org.gamja.gamzatechblog.core.annotation.ApiController;
+import org.gamja.gamzatechblog.domain.tag.controller.docs.TagControllerSwagger;
 import org.gamja.gamzatechblog.domain.tag.service.TagService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 
 @ApiController("/api/v1/tags")
 @RequiredArgsConstructor
-public class TagController {
+public class TagController implements TagControllerSwagger {
 	private final TagService tagService;
 
-	@Operation(summary = "전체 태그 조회", tags = "태그 기능")
 	@GetMapping
 	public ResponseDto<List<String>> getAllTags() {
 		List<String> tags = tagService.getAllTags();
