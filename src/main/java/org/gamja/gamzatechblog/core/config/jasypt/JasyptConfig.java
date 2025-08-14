6ce19aa8566c1a50d5e27d3b748edd3c9e449d81
박지훈 <jihun6548@naver.com>
2025-08-14 22:04:05 +0900

    chore: build.gradle 수정 및 Jasypt 설정 변경

--- src/main/java/org/gamja/gamzatechblog/core/config/jasypt/JasyptConfig.java
+++ src/main/java/org/gamja/gamzatechblog/core/config/jasypt/JasyptConfig.java
@@ -1,4 +1,4 @@
-package org.gamja.gamzatechblog.core.config.jasypt;
+package org.gamja.gamzatechblog.core.config;
 
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
