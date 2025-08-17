e2e9b3028ae788dc6f7b9650f3cad8a5848b75fb
박지훈 <jihun6548@naver.com>
2025-08-17 21:24:35 +0900

    feat: 댓글 생성/삭제 요청을 위한 DTO 추가 (CreateCommentCommand, DeleteCommentCommand)

--- src/main/java/org/gamja/gamzatechblog/domain/comment/model/dto/CreateCommentCommand.java
+++ src/main/java/org/gamja/gamzatechblog/domain/comment/model/dto/CreateCommentCommand.java
@@ -1,4 +1,15 @@
 package org.gamja.gamzatechblog.domain.comment.model.dto;
 
-public class CreateCommentCommand {
+import org.gamja.gamzatechblog.domain.comment.model.dto.request.CommentRequest;
+import org.gamja.gamzatechblog.domain.user.model.entity.User;
+
+public record CreateCommentCommand(
+	User user,
+	Long postId,
+	String content,
+	Long parentCommentId
+) {
+	public static CreateCommentCommand of(User user, Long postId, CommentRequest req) {
+		return new CreateCommentCommand(user, postId, req.content(), req.parentCommentId());
+	}
 }
