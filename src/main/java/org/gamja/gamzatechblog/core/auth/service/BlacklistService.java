d7caf506fc28bddafc817bc71b28c303fdc974ab
박지훈 <jihun6548@naver.com>
2026-06-27 20:53:09 +0900

    refactor: 토큰 검증기 생성자 주입 통일 및 블랙리스트 키 상수화

--- src/main/java/org/gamja/gamzatechblog/core/auth/service/BlacklistService.java
+++ src/main/java/org/gamja/gamzatechblog/core/auth/service/BlacklistService.java
@@ -1,5 +1,7 @@
 package org.gamja.gamzatechblog.core.auth.service;
 
 public interface BlacklistService {
+	String BLACKLIST_ACCESS_PREFIX = "blacklist:access:";
+
 	void blacklistTokens(String githubId);
 }
